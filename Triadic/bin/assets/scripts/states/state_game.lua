d74073f7a50f1f0b48ae6886e5c51472116f13ee
Jaddori <ohlsson.niclas@gmail.com>
2018-10-09 21:40:06 +0200

    Revert "Merge branch 'editor' into dev"
    This reverts commit 9240ff0b2327472f14b281e14f484b2e16bf2d95, reversing
    changes made to 87b8f6a630e499a3bf58f99030dfc434a7095394.

--- Triadic/bin/assets/scripts/states/state_game.lua
+++ Triadic/bin/assets/scripts/states/state_game.lua
@@ -72,5 +72,5 @@ function StateGame:serverRead()
 	GameServer:serverRead()
 end
 
---Game:addState( StateGame )
---Game:setState( StateGame.name )
+Game:addState( StateGame )
+Game:setState( StateGame.name )
